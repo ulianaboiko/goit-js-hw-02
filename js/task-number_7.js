3e58a67bf2bc14bee66014b920b8a7cb5c9f8f97
ulianaboiko <157517980+ulianaboiko@users.noreply.github.com>
2025-03-04 11:55:37 +0200

    complited one more task

---
+++ js/task-number_7.js
@@ -0,0 +1,29 @@
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+const isLoginValid = function (login) {
+  if (login.length >= 4 && login.length <= 16) {
+    return true;
+  }
+};
+
+const isLoginUnique = function (allLogins, login) {
+  if (!allLogins.includes(login)) {
+    return true;
+  }
+};
+
+const addLogin = function (allLogins, login) {
+  if (!isLoginValid(login)) {
+    return "Помилка! Логін повинен бути від 4 до 16 символів";
+  }
+  if (!isLoginUnique(allLogins, login)) {
+    return "Такий логін вже використовується!";
+  }
+  logins.push(login);
+  return "Логін успішно доданий!";
+};
+
+console.log(addLogin(logins, "Ajax")); // 'Логін успішно доданий!'
+console.log(addLogin(logins, "robotGoogles")); // 'Такий логін вже використовується!'
+console.log(addLogin(logins, "Zod")); // 'Помилка! Логін повинен бути від 4 до 16 символів'
+console.log(addLogin(logins, "jqueryisextremelyfast")); // 'Помилка! Логін повинен бути від 4 до 16 символів'
